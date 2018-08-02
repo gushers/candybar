9d66ea624754185b0cfe145d9566aff33a8f37b4
J Smith <jscsmith@gmail.com>
2018-08-01 23:13:19 -0700

    fix path

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Entity from './modules/Entity';
 import Point from './modules/Point';
 import Pointer from './modules/Pointer';
 import Velocity from './modules/Velocity';
-import * as utils from './modules/utils';
+import * as utils from './utils';
 
 export default {
     Background,
