a43217e041bb676ea3494cb22cf4315001967468
J Smith <jscsmith@gmail.com>
2018-08-01 23:16:16 -0700

    add delta properties

--- src/modules/Pointer.js
+++ src/modules/Pointer.js
@@ -7,7 +7,9 @@ import Point from './Point';
 class Pointer {
     constructor() {
         this.dpr = window.devicePixelRatio || 1;
-        this.position = new Point(0, 0);
+        this.delta;
+        this.lastPosition = null;
+        this.position = new Point(null, null);
         this.addListeners();
     }
 
@@ -20,14 +22,32 @@ class Pointer {
             window.addEventListener(
                 event,
                 e => {
+                    // move previous point
+                    const { x: px, y: py } = this.position;
+
+                    // disable the demo modifier if it's been added
+                    if (this.modifier) {
+                        this.modifier = null;
+                    }
+
                     if (touch) {
                         e.preventDefault();
                         const x = e.targetTouches[0].clientX * this.dpr;
                         const y = e.targetTouches[0].clientY * this.dpr;
+                        if (!this.lastPosition) {
+                            this.lastPosition = new Point(x, y);
+                        } else {
+                            this.lastPosition.moveTo(px, py);
+                        }
                         this.position.moveTo(x, y);
                     } else {
                         const x = e.clientX * this.dpr;
                         const y = e.clientY * this.dpr;
+                        if (!this.lastPosition) {
+                            this.lastPosition = new Point(x, y);
+                        } else {
+                            this.lastPosition.moveTo(px, py);
+                        }
                         this.position.moveTo(x, y);
                     }
                 },
