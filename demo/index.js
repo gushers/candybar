6c5603f98d870eb0c8be29163e1c7c5c72dab574
J Smith <jscsmith@gmail.com>
2018-08-01 22:44:41 -0700

    updating demo

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,12 @@
-import Canvas from './modules/Canvas';
-import Background from './modules/Background';
-import Cursor from './modules/Cursor';
-import Pointer from './modules/Pointer';
+import Canvas from '../src/modules/Canvas';
+import Background from '../src/modules/Background';
+import Cursor from '../src/modules/Cursor';
+import Pointer from '../src/modules/Pointer';
 
 // Kick off
 const canvas = new Canvas({
     canvas: document.getElementById('canvas'),
+    container: document.getElementById('container'),
     pointer: new Pointer(),
     entities: [new Background(), new Cursor(10)],
 });
